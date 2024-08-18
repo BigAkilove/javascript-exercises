2ab16d4a585922967a02eba860d4077265ee127c
Valou <anonanonymous018@gmail.com>
2024-08-18 21:18:40 +0200

    Do Javascript exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if(!string) return ''
+    
+    let reversedString = '';
+    let i = string.length;
+    
+    do {
+        i--;
+        reversedString += string[i];
+    } while (i>0)
+    return reversedString
 };
 
 // Do not edit below this line

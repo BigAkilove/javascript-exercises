db172eab3d3a07f08777f341bd227b3912efaab7
Valou <anonanonymous018@gmail.com>
2024-08-19 17:01:05 +0200

    do exercice 5, one check is left

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let result = 0, 
+    maxValue= Math.max(...args), 
+    minValue = Math.min(...args);
 
+    if (!Number.isInteger(maxValue) || !Number.isInteger(minValue)|| maxValue<0 || minValue<0 || (minValue % 1 != 0) || (maxValue %1 != 0)) {
+        return 'ERROR' 
+    } else {
+        while (maxValue>minValue) {
+        result += maxValue;
+        maxValue-- 
+    }
+    result+= minValue
+    return result
 };
-
+}
 // Do not edit below this line
 module.exports = sumAll;

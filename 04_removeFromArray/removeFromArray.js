8f99e09180477d2d6e0dc2fec39281250cdbc23e
Valou <anonanonymous018@gmail.com>
2024-08-19 14:46:34 +0200

    Do exercice 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, itemToRemove) {
+    const args = Array.from(arguments)
+    args.shift()
+    console.log('args =' + args)
+    let newArray = [];
+
+    label: for(let i=0; i<array.length ; i++) {
+        analysedItem = array[i];
+ 
+        for(let i=0; i<args.length; i++) {
+            filterItem = args[i];
+            console.log('analysedItem= ' + analysedItem, 'filterItem= ' +filterItem)
+            if (analysedItem === filterItem) continue label
+        }
+        newArray.push(analysedItem);
+    }
+
+    return newArray
 };
 
 // Do not edit below this line

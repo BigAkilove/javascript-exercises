c65abc152dd21382ef5aeac7d3d2297299f63733
Valou <anonanonymous018@gmail.com>
2024-09-06 15:52:21 +0200

    do the 11 exercise with an array of objects with the foreach method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let returnArray = []
+    books.forEach(book => {
+        returnArray.push(book.title)
+    });
+    return returnArray
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;

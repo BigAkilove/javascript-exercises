b2e7ac793ce72f3f9b0867f27dcc2f5e515c3de6
Valou <anonanonymous018@gmail.com>
2024-09-06 13:43:49 +0200

    do exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,28 +1,50 @@
-const add = function() {
-	
+let array1 = [2,4]
+const add = function(a,b) {
+	if (Array.isArray(a)) {
+    return a.reduce((total, item) => total+=item,0)
+  }
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a, ...args) {
+  let newArray = a.concat(args)
+  console.log(newArray)
+	if (Array.isArray(a)) {
+    return newArray.reduce((total, item) => total+=item,0)
+  };
+}
+
+const multiply = function(a) {
+  return a.reduce((total, item) => total * item,1)
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const power = function() {
-	
+const factorial = function(a) {
+	let sum = a
+  let i = a-1
+  
+  //for (; i<=1; i--) {
+  //  console.log(sum,i)
+  //  sum = sum + a*i
+  //}
+  while(i>=1) {
+    console.log(sum,a,i)
+    sum = sum * i
+    i =  i-1
+  }
+  if (sum === 0) return 1
+  return sum
 };
 
-const factorial = function() {
-	
-};
+//Do not edit below this line
 
-// Do not edit below this line
 module.exports = {
   add,
   subtract,

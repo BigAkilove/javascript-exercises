77adaaf57031aa20a6428f72225e9669398d4894
Valou <anonanonymous018@gmail.com>
2024-09-06 17:05:49 +0200

    do exercise 12 but the solution is very different

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let returningArray = []
+    
+    people.forEach((person,index) => {
 
+        let personNumber = index
+        let birthYear = person.yearOfBirth;
+        let deathYear = person.yearOfDeath;
+
+        if (!deathYear) {
+            deathYear = new Date
+            deathYear = deathYear.getFullYear()
+        }
+        let age = deathYear - birthYear
+        returningArray.push(personNumber,age)
+    })
+
+    return people[returningArray[returningArray.indexOf(Math.max(...returningArray))-1]]
 };
 
 // Do not edit below this line

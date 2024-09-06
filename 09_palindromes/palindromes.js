b2071edac4c6aaa69127174a7824c6c4b9b368e6
Valou <anonanonymous018@gmail.com>
2024-09-06 13:42:17 +0200

    remove punctuation to check palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let noSpace = string.replaceAll(new RegExp(/[\s\.,!]/g),'').toLowerCase();
+    let reversedString = ''
 
+    for (let i = noSpace.length -1; i>-1;i--) {
+        console.log(i, typeof noSpace[i]);
+        reversedString = reversedString.concat(noSpace[i]);
+    }
+    return reversedString === noSpace
 };
 
 // Do not edit below this line

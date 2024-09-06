e8650834d40e4ce715f2498c229736cd3ae3b67e
Valou <anonanonymous018@gmail.com>
2024-09-06 15:05:41 +0200

    do the 10 fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let numberParam = Number(number)
+    
+    if (numberParam === 0) {
+        return 0 
+    } else if (numberParam<0) {
+        return 'OOPS'
+    } else {
+        let startingArray = [1,1]
+        console.log(startingArray)
+        for (let i = 0; i <= numberParam-1; i++) {
+            startingArray.push(startingArray[i] + startingArray[i+1])
+        }
+        return startingArray[numberParam-1]
+    }
+    
 };
 
+console.log(fibonacci(3))
+console.log(fibonacci(10))
+console.log(fibonacci(-10))
 // Do not edit below this line
-module.exports = fibonacci;
+//npm test fibonacci.spec.js 
+//module.exports = fibonacci;
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
